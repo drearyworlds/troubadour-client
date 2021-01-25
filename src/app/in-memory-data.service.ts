5cb380bfb8090dc5f2f673451d2432b19aac9b4c
Jim Craft <james.craft@bluehalo.com>
2021-01-25 12:24:26 -0600

    data coming from in-memory server

---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Song } from './song';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const songs = [
+      {
+        id: 1,
+        title: 'Doing the Unstuck',
+        artist: 'The Cure',
+        album: 'Wish',
+        year: 1992,
+        active: true
+    },{
+        id: 2,
+        title: 'Friday, I\'m in Love',
+        artist: 'The Cure',
+        album: 'Wish',
+        year: 1992,
+        active: true
+    }
+    ];
+    return {songs};
+  }
+
+  // Overrides the genId method to ensure that a hero always has an id.
+  // If the heroes array is empty,
+  // the method below returns the initial number (11).
+  // if the heroes array is not empty, the method below returns the highest
+  // hero id + 1.
+  genId(songs: Song[]): number {
+    return songs.length > 0 ? Math.max(...songs.map(song => song.id)) + 1 : 11;
+  }
+}
