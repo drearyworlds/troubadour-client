d23860f2bdf5e783c219185dbab10cec119da9f6
Jim Craft <jimcraft@live.com>
2021-02-13 16:29:11 -0600

    Rename project name references

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/current-song'),
+      dir: require('path').join(__dirname, './coverage/songlistclient'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'current-song'`, () => {
+  it(`should have as title 'songlistclient'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('current-song');
+    expect(app.title).toEqual('songlistclient');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('current-song app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('songlistclient app is running!');
   });
 });
