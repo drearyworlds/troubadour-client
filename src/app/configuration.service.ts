15f44990f59acdcb5f3a1a9719d1932f1685f238
Jim Craft <jimcraft@live.com>
2021-05-22 23:58:01 -0500

    .

--- src/app/configuration.service.ts
+++ src/app/configuration.service.ts
@@ -7,7 +7,7 @@ export class ConfigurationService {
     // Note: These values are not currently being read from the config.json
     public serverHost: string = "localhost";
     public serverPort: number = 3000;
-    public editMode: boolean = true;
+    public editMode: boolean = false;
 
     constructor() {
     }
